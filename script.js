75aa72a2d1c21bd928b9a8e5b76bb618f4fb77b9
Hasan Masud <hmmasud635@gmail.com>
2025-01-21 17:23:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+// let age = prompt("Enter your age");
+
+// if (age >=18) {
+//     console.log("Apni NID korte parben")
+// }
+// else console.log("Apnar age kom, apni NID korte parben na");
+
+// .....................................................
+
+// let student = prompt("Hello");
+
+// if (student=="Assalamu Alaikum") {
+//     console.log("Walaikumus Salam");
+
+//     let student = prompt("Apni ki MERN Stock er student?");
+
+//     if (student == "yes") {
+//         console.log("Ok Thanks");
+
+//         let student = prompt("Apnar batch number bolen");
+
+//         if (student == "2405") {
+//             console.log("Apnar room number Theory-07");
+//         }
+//         else console.log("Help desk a kotha bolen");
+//     }
+//     else console.log("Apni ki admission nite chan?");
+// }
+// else console.log("Apni akhon aste paren");
+
+// ....................................................
+
+// var number = prompt("Enter your Marksheet Number");
+
+// if (number >= 80) {
+//     console.log("A+")
+// } else
+// if (number >= 70) {
+//     console.log("A")
+// } else
+// if (number >= 60) {
+//     console.log("A-")
+// } else
+// if (number >= 50) {
+//     console.log("B")
+// } else
+// if (number >= 40) {
+//     console.log("c")
+// } else
+// if (number >= 33) {
+//     console.log("D")
+// }
+// else console.log("F")
+
+// .....................................................
+
+// var number = prompt("number is")
+
+// let result = Number(number)
+
+// switch (result) {
+//     case 10:
+//         console.log("number is 10")
+//         break;
+
+//     case 20:
+//         console.log("number is 20")
+//         break;
+
+//     case 30:
+//         console.log("number is 20")
+//         break;
+
+//     default:
+//         console.log("not match")
+//         break;
+// }
+
+// ......................................................
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("2 X " + i + " = " + 2 * i);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`3 X ${i} = ${3 * i}`);
+// }
+
+
+// var number = 4;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} X ${i} = ${number * i}`);
+// }
+
+
+// var number = prompt("Enter your Multipication Number");
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} X ${i} = ${number * i}`);
+// }
+
+
+// ......................................................
+
+// var user;
+
+// console.log(user)
+
+// var user = null;
+
+// console.log(user)
+
+// ......................................................
+
+// class: JavaScript class=5
+
+
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(i)
+
+//     i++
+// }
+
+//..........................................................
+
+// let value = prompt("Enter your text")
+
+// while (value !=="exit") {
+//     console.log(value)
+// }
+
+
+// function declaration....................................
+
+// let numberone = prompt("Enter your 1st number")
+// let numbertwo = prompt("Enter your 2nd number")
+
+// function sum (a, b) {
+//     console.log(Number(a) + Number(b))
+// }
+
+// sum(numberone, numbertwo);
+
+
+// function expression.......................................
+
+// let jog = function (a, b) {
+//     console.log("jog")
+// }
+
+// jog();
+
+// rest......................................................
+
+// function sum (a, ...b) {
+//     console.log(a, b)
+// }
+
+// sum(20, 30, 50, 60, 80, 90);
